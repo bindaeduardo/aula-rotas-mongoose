96b9b65b34e468a3c5e1e8e7decadc81cb03565b
comoquepode <eduardobinda7@gmail.com>
2015-10-24 11:28:22 -0200

    Commit controller.js

---
+++ controller.js
@@ -0,0 +1,78 @@
+var Model = require('./model');
+
+var Controller = {
+  	create:function (req,res){
+  		console.log('create');
+       var dados = { name: 'Skol', desciption: 'opa', alchool:9.0, price:90.0,category:'pilsen'};
+	   var model = new Model(dados); 
+	   var msg = '';
+
+	  	model.save( function(err, data) { 
+			if (err) { 
+			    console.log('Erro:' , err );
+			    msg = 'Erro'+ err; 
+			} else {
+			  console.log('Cerveja inserida:', data); 
+			  msg = 'Cerveja inserida '+data;
+			}
+			res.end(msg);
+		});
+  	},
+  	retrieve:function (req,res){
+  		console.log('retrieve');
+		var query = {};
+		Model.find(query, function(err, data) { 
+		if (err) { 
+		    console.log('Erro:' , err );
+		    msg = 'Erro'+ err; 
+		} else {
+			console.log('Listagem:', data); 
+			msg = 'Listagem efetuada '+ data;
+		}		
+		res.end(msg);
+	});
+  	},
+  	update:function (req,res){
+		 
+        var query = {name: /skol/i};
+		
+		var mod = {
+			name:'Brahma',
+			description:'Masomenote'
+		};
+
+		var optional = {
+			upsert:false,
+			multi:true
+		};
+
+		Model.update(query,mod,optional,function(err,data){
+			if(err){
+				console.log('Erro',err);
+				msg = 'Erro'+ err; 
+			}
+			else{
+				console.log('Cervejas atualizadas com sucesso...', data);
+				msg = 'Cervejas atualizadas com sucesso '+ data;
+			}
+			res.end(msg);
+		});
+ 	},
+  	delete:function (req,res){
+		var query = {name: /brahma/i} ;
+
+		Model.remove(query,function(err,data){
+			if(err){
+				console.log('Erro', err);
+				msg = 'Erro'+ err; 
+			}
+			else{
+				console.log('Cerveja deletada com sucesso, quantidade', data.result);
+				msg = 'Cerveja deletada com sucesso '+ data;
+		    }			
+		 res.end(msg);
+		});
+	}  	
+  };
+
+module.exports = Controller;
