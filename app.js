951497dd57790b13d33e916a61ad7df1f4564c4a
comoquepode <eduardobinda7@gmail.com>
2015-10-24 11:18:08 -0200

    Modificações app.js e model.js

--- app.js
+++ app.js
@@ -1,47 +1,109 @@
 var http = require('http');
+var Model = require('./model');
 
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/pos-unoesc');
+var Controller = {
+  	create:function (req,res){
+  		console.log('create');
+       var dados = { name: 'Skol', desciption: 'opa', alchool:9.0, price:90.0,category:'pilsen'};
+	   var model = new Model(dados); 
+	   var msg = '';
 
-var db = mongoose.connection;
+	  	model.save( function(err, data) { 
+			if (err) { 
+			    console.log('Erro:' , err );
+			    msg = 'Erro'+ err; 
+			} else {
+			  console.log('Cerveja inserida:', data); 
+			  msg = 'Cerveja inserida '+data;
+			}
+			res.end(msg);
+		});
+  	},
+  	retrieve:function (req,res){
+  		console.log('retrieve');
+		var query = {};
+		Model.find(query, function(err, data) { 
+		if (err) { 
+		    console.log('Erro:' , err );
+		    msg = 'Erro'+ err; 
+		} else {
+			console.log('Listagem:', data); 
+			msg = 'Listagem efetuada '+ data;
+		}		
+		res.end(msg);
+	});
+  	},
+  	update:function (req,res){
+		 
+        var query = {name: /skol/i};
+		
+		var mod = {
+			name:'Brahma',
+			description:'Masomenote'
+		};
 
-db.on('error', function(err){ console.log('Erro de conexao.',err ) ;});
-                             
-db.on('open', function(){ console.log('Conexão Aberta') });
-                             
-db.on('conected', function(err){ console.log('Conectado') });
-                             
-db.on('disconnected', function(){ console.log('Desconectado') });                 
+		var optional = {
+			upsert:false,
+			multi:true
+		};
 
-var Schema = mongoose.Schema;
+		Model.update(query,mod,optional,function(err,data){
+			if(err){
+				console.log('Erro',err);
+				msg = 'Erro'+ err; 
+			}
+			else{
+				console.log('Cervejas atualizadas com sucesso...', data);
+				msg = 'Cervejas atualizadas com sucesso '+ data;
+			}
+			res.end(msg);
+		});
+ 	},
+  	delete:function (req,res){
+		var query = {name: /brahma/i} ;
 
-var json_schema = {  name: {type:String, default:''},
-                   description: {type :String, default:''} };
-
-var BeerSchema = new Schema(json_schema);
-
-var Beer = mongoose.model('Beer', BeerSchema);
-
-
-
+		Model.remove(query,function(err,data){
+			if(err){
+				console.log('Erro', err);
+				msg = 'Erro'+ err; 
+			}
+			else{
+				console.log('Cerveja deletada com sucesso, quantidade', data.result);
+				msg = 'Cerveja deletada com sucesso '+ data;
+		    }			
+		 res.end(msg);
+		});
+	}  	
+  };
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  var dados = { name: 'Skol', desciption: 'opa'};
 
-var model = new Beer(dados), msg = '';
+  var url = req.url;
+  var msg = '';
 
-model.save( function(err, data) { 
-if (err) { 
-    console.log('Erro:' , err );
-    msg = 'Erro'+ err; 
-} else {
-  console.log('Cerveja inserida:', data); 
-  msg = 'Cerveja inserida '+data;
-}
- res.end(msg);
-} );
+  switch(url){
+  	case '/api/beers/create':
+  		console.log('switch create');
+	     Controller.create(req,res);  	
+  	break;
+  	case '/api/beers/retrieve':
+  		console.log('switch retrieve');
+  		Controller.retrieve(req,res);
+  	break;
+  	case '/api/beers/update':
+  		Controller.update(req,res);
+  	break;
+  	case '/api/beers/delete':
+  		Controller.delete(req,res);
+  	break;
+  	default:
+  	   res.end('Rota não encontrada');
+  	break;
+  }
+ 
+}).listen(3000);		
 
-}).listen(3000);
 
-console.log('Server running at http://127.0.0.1:3000/');
+
+  
---
+++ model.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/pos-unoesc');
+
+var db = mongoose.connection;
+
+db.on('error', function(err){ console.log('Erro de conexao.',err ) ;});
+                             
+db.on('open', function(){ console.log('Conexão Aberta') });
+                             
+db.on('conected', function(err){ console.log('Conectado') });
+                             
+db.on('disconnected', function(){ console.log('Desconectado') });                 
+
+var Schema = mongoose.Schema;
+
+var json_schema = {  name:{type:String, default:''}
+					,description:{type:String, default:''}
+					,alchool:{type:Number,min:0}
+					,price:{type:Number,min:0}
+					,category:{type:String,default:''}
+					,created_at:{type:Date,default:Date.now()}
+                  }
+
+var BeerSchema = new Schema(json_schema);
+
+module.exports = mongoose.model('Beer', BeerSchema);
+
